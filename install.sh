b4b8503d18f9fe666cdcfcb3e531ad8cc45f8da5
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2020-02-17 20:58:42 +0100

    Added auto reload of ~/.zshrc

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ current=$(pwd)
 # Link the file 'sebastiaanfranken.zsh-theme' to ~/.oh-my-zsh/themes/
 # and source the current zshrc file.
 ln -sv ${current}/sebastiaanfranken.zsh-theme ${HOME}/.oh-my-zsh/themes/
+sed -i 's/^ZSH_THEME=*/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
 source ~/.zshrc
 
 # Exit the script
