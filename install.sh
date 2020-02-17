dd0b783a20622aee66909dc518e8f6e3cd6419bf
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2020-02-17 21:09:17 +0100

    Reworked install script

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ current=$(pwd)
 
 # Link the file 'sebastiaanfranken.zsh-theme' to ~/.oh-my-zsh/themes/
 # and source the current zshrc file.
+if [[ -f ~/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme ]]; then
+	rm ${HOME}/.oh-my-zsh/theme/sebastiaanfranken.zsh-theme
+fi
+
 ln -sv ${current}/sebastiaanfranken.zsh-theme ${HOME}/.oh-my-zsh/themes/
-#sed -i 's/^ZSH_THEME=*/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
 source ~/.zshrc
 
