0076e8160711c47d8a7f560f0e91fc4eeb1d805a
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2020-02-17 23:59:14 +0100

    Move theme link to custom/themes

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 # The current working directory
 current=$(pwd)
 
-# If there is a file ~/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme remove it
-if [[ -f ~/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme ]]; then
-	rm ${HOME}/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme
+# If there is a file ~/.oh-my-zsh/custom/themes/sebastiaanfranken.zsh-theme remove it
+if [[ -f ~/.oh-my-zsh/custom/themes/sebastiaanfranken.zsh-theme ]]; then
+	rm ${HOME}/.oh-my-zsh/custom/themes/sebastiaanfranken.zsh-theme
 fi
 
 # Link the sebastiaanfranken.zsh-theme file into ~/.oh-my-zsh/themes/
 # and source the ~/.zshrc file
-ln -sv ${current}/sebastiaanfranken.zsh-theme ${HOME}/.oh-my-zsh/themes/
+ln -sv ${current}/sebastiaanfranken.zsh-theme ${HOME}/.oh-my-zsh/custom/themes/
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
 source ~/.zshrc
 
