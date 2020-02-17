5bd471eef5e6ea99468771a2f778d4ea34ee8309
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2020-02-17 21:04:56 +0100

    Reworked sed

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ current=$(pwd)
 # Link the file 'sebastiaanfranken.zsh-theme' to ~/.oh-my-zsh/themes/
 # and source the current zshrc file.
 ln -sv ${current}/sebastiaanfranken.zsh-theme ${HOME}/.oh-my-zsh/themes/
-sed -i 's/^ZSH_THEME=*/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
+#sed -i 's/^ZSH_THEME=*/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
+sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
 source ~/.zshrc
 
 # Exit the script
