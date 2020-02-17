ad57c5174fb9473a09af969304045f7344010bdd
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2020-02-17 21:12:16 +0100

    Final rework

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 # The current working directory
 current=$(pwd)
 
-# Link the file 'sebastiaanfranken.zsh-theme' to ~/.oh-my-zsh/themes/
-# and source the current zshrc file.
+# If there is a file ~/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme remove it
 if [[ -f ~/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme ]]; then
-	rm ${HOME}/.oh-my-zsh/theme/sebastiaanfranken.zsh-theme
+	rm ${HOME}/.oh-my-zsh/themes/sebastiaanfranken.zsh-theme
 fi
 
+# Link the sebastiaanfranken.zsh-theme file into ~/.oh-my-zsh/themes/
+# and source the ~/.zshrc file
 ln -sv ${current}/sebastiaanfranken.zsh-theme ${HOME}/.oh-my-zsh/themes/
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="sebastiaanfranken"/' ~/.zshrc
 source ~/.zshrc
